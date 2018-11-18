be3a565fff7c66210bcd15d2cdf059ace90fa2f9
Flizzet <pedro@flizzet.com>
2018-11-18 08:54:51 -0800

    Initial commit

---
+++ spec/flizz-badge-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FlizzBadgeView from '../lib/flizz-badge-view';
+
+describe('FlizzBadgeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
